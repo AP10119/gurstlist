ff38fef58f83e5985939d3fbc01e96e27081161d
AP10119 <75711027+AP10119@users.noreply.github.com>
2021-06-02 21:04:10 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,31 +1,25 @@
 var array10 = [];
 var length;
-var empty_array= [];
 function submit(){
-for(var i=1; i<=3; i++){
-   var name= document.getElementById("name_of_the_guest"+i).value;
+   var name= document.getElementById("name_of_the_guest1").value;
    array10.push(name);
-}
 console.log(array10);
 length=array10.length;
-for(var j=0;j<length;j++){
-empty_array.push("name-"+array10[j]);
-console.log(empty_array);
-}
-document.getElementById("Guestswithcomma").innerHTML=empty_array;
-var with_commas= empty_array.join(" ");
-console.log(with_commas);
-document.getElementById("submit_button").style.display="none";
-document.getElementById("sort_button").style.display="inline-block";
+document.getElementById("Guestswithcomma").innerHTML=array10;
 }
 function sorting(){
-    var sorted_array = [];
     array10.sort();
-    var length1 =array10.length;
-    for(var k=0; k<length1; k++){
-        sorted_array.push("name-"+array10[k]);
+    var sorted_array = array10.join("<br>")                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                       
+    document.getElementById("sorted").innerHTML=sorted_array.toString();
+}
+function search(){
+    var namesearched=document.getElementById("search_the_guest1").value;
+    var found=0;
+    for(var i=0;i<array10.length;i++){
+        if(namesearched==array10[i]){
+            found=found+1;
+        }
     }
-    document.getElementById("Guestswithcomma").innerHTML=sorted_array;
-var without_commas= sorted_array.join(" ");
-console.log(without_commas);
+    document.getElementById("found").innerHTML="namefound"+found;
+    
 }
